3f9918b66e3e740261fa1edab244ad00e75cb5eb
Healy Dwyer <healyheels@gmail.com>
2016-04-09 22:22:52 -0400

    Added files via upload

---
+++ Letter.py
@@ -0,0 +1,4 @@
+for int in range (100,1):
+    int = 99
+    print int + "bottles of beer on the wall," + int + "bottles of beer, you take one down and pass it around, " + (int- 1) + "bottles of beer on the wall. "
+    
---
+++ bottles.py
@@ -0,0 +1,10 @@
+for num in range (100, 1, -1):
+    print str(num) + " bottles of beer on the wall," + str(num) + " bottles of beer, you take one down and pass it around, " + str(num-1) + " bottles of beer on the wall. "
+    
+
+print str(num) + " bottles of beer on the wall, " + str(num) + " bottles of beer, you take one down and pass it around," + str(num-1) + " bottle of beer on the wall."
+
+print "1 bottle of beer on the wall, 1 bottle of beer, you take one down and pass it around, 0 bottles of beer on the wall."
+    
+    
+print "The End!"
